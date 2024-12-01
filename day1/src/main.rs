d73663b55e57fc58fcaf637eac6c4b22ce4bdfd8
Javier Martinez Alcantara <jmaralc@gmail.com>
2024-12-01 22:36:58 +0100

    test: summing up a vector

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -7,8 +7,12 @@ pub fn short_list(mut list: Vec<i32>) -> Vec<i32> {
     list.sort();
     list
 }
-pub fn element_wise_distance(mut vec1: Vec<i32>, mut vec2: Vec<i32>,) -> Vec<i32> {
+pub fn element_wise_distance(vec1: Vec<i32>, vec2: Vec<i32>,) -> Vec<i32> {
     vec1.iter().zip(vec2.iter()).map(|(&b, &v)| (b - v).abs()).collect()
+}
+
+pub fn sum_vector(vec: Vec<i32>) -> i32 {
+    vec.iter().sum()
 }
 
 
@@ -31,7 +35,7 @@ mod tests {
 
 
     #[test]
-    fn substract_vectors_should_return_the_element_wise_substraction() {
+    fn element_wise_distance_should_return_the_element_wise_substraction() {
         // Given
         let vector1 =  vec![1,2,3,3,3,4];
         let vector2 =  vec![3,3,3,4,5,9];
@@ -40,6 +44,20 @@ mod tests {
 
         // When
         let result = element_wise_distance(vector1, vector2);
+
+        // Then
+        assert_eq!(result, expected_result);
+    }
+
+    #[test]
+    fn sum_vector_should_sum_all_the_elements_of_a_vector() {
+        // Given
+        let vector =  vec![2,1,0,1,2,5];
+
+        let expected_result = 11;
+
+        // When
+        let result = sum_vector(vector);
 
         // Then
         assert_eq!(result, expected_result);
